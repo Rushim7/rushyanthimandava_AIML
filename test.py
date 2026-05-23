0696fe00b4bdadc80251b19bbc09df0ad9ae2041
Rushim7 <mandavars.outlook.com>
2026-05-23 14:36:08 -0400

    Initial commit

---
+++ test.py
@@ -0,0 +1,25 @@
+#Task1:
+
+for i in range(1,6):
+  print(i)
+
+
+#Task2:
+
+for i in range(1,11,2):
+ print(i)
+
+
+#Task3:
+
+for i in range(3):
+ for j in range(2):
+  print(i,j)
+
+ 
+#Task4:
+
+for i in range(1,8):
+ print(i)
+ if i ==5:
+  break
